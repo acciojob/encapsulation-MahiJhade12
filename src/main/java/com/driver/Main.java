7f525d85fdc26bb8effd83e8e92493f317050d67
mahima <mahimajhade123@gmail.com>
2022-12-13 14:23:18 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly ro=new RWOnly();
+    ro.setName("mahima");
+    System.out.println(ro.getName());
+  }
 }
